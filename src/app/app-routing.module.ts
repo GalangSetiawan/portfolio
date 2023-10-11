30ff957707c79cfd49007e449184925b482afaba
Galang Setiawan <52562711+GalangSetiawan@users.noreply.github.com>
2023-10-11 07:25:37 +0000

    tambah file home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
+
 import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// import { authInterceptorProviders } from './_helpers/auth.interceptor.service';
+// import { AuthGuard } from './_helpers/auth.guard';
+// import { HttpClientModule } from '@angular/common/http';
+
+
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+  ],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
